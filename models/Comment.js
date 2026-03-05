a7e8db39e2ed0e9848de777b1f022de2ad3337a4
Julian Carrico <Julian.carrico@gmail.com>
2026-03-05 14:51:04 -0500

    added comments to binary-upload-bloom

---
+++ models/Comment.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { post } = require("../routes/main");
+
+const CommentSchema = new mongoose.Schema({
+    comment: {
+        type: String,
+        required: true,
+    },
+    likes: {
+        type: Number,
+        required: true,
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
